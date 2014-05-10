0ca80e6fe605c47cbd9d6026d4eae8416053f000
Roger Creasy <roger.creasy@gmail.com>
2014-05-10 18:27:29 -0400

    add rango sub-directory

--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+import rango.urls
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tango_with_django.views.home', name='home'),
+    url(r'^$', 'tango_with_django.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^rango/', include(rango.urls)),
 )
